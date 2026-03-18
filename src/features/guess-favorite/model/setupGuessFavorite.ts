7361c6fffb81541fe492b82716af04390de24777
SonMyeongJin <smj2802@naver.com>
2026-03-18 15:20:37 +0900

    [Comment] setupGuessFavorite.ts の一行一行説明コメント化

--- src/features/guess-favorite/model/setupGuessFavorite.ts
+++ src/features/guess-favorite/model/setupGuessFavorite.ts
@@ -1,7 +1,7 @@
 import { getPokemon, type Pokemon } from '../../../entities/pokemon/api';
 
+// 変わらない変数だと思って外に出す。
 const correctPokemon = 387; // 387: turtwig / ナエトル / 모부기
-
 const hint: string[] = [
   'This pokemon is a grass type.',
   'This pokemon is the first stage of its evolution line.',
@@ -11,50 +11,61 @@ const hint: string[] = [
   'なんとなく387回挑戦すれば正解に近づくかもしれない? ^_^',
 ];
 
-const form = document.getElementById('pokemon-form');
-
-const input = document.getElementById('pokemon-input'); // as HTMLInputElement;
-const doko = document.getElementById('pokemon-body');
-
+// DomLoaderによって呼ばれるとき（一回だけ）呼ばれる。
 function setupGuessFavorite() {
+  // id="~"を持っている要素を持ってくる。形はHTMLFormElement！
+  const form = document.getElementById('pokemon-form');
+  // as Element 使わなくで安定を検査する。
   if (!(form instanceof HTMLFormElement)) {
     return;
   }
-
+  // formにsubmitイベントが起きたら、非同期関数を実行する。
   form.addEventListener('submit', onGuessSubmitHandler);
 }
 
+// 答えを入力してsubmitしたときの処理。答えが正しいかどうかを判断する。
 async function onGuessSubmitHandler(e: SubmitEvent) {
+  // submitイベントのデフォルトの動きを止める。例えば、ページのリロードとか。
   e.preventDefault();
 
+  // 使用者が入力するinput要素を変数に持って入れる。
+  const input = document.getElementById('pokemon-input');
+  // 使用者が入力したPokemonの情報を見せてあげるElement
+  const inputPokemonInfoLocation = document.getElementById('pokemon-body');
   if (!(input instanceof HTMLInputElement)) {
     return;
   }
-  if (!(doko instanceof HTMLElement)) {
+  if (!(inputPokemonInfoLocation instanceof HTMLElement)) {
     return;
   }
-
-  console.log(input.value);
+  // 使用者が入力した値のPokemon Objectを持ってくる
   const pokemon = await getPokemon(input.value);
+  console.log(input.value);
   console.log(pokemon.name);
 
-  // 下の数呼ぶ。dokoに！マークは ”dokoがnullわけない”ということをCompilerに教えてあげる。
-  //（Compile段階ではErrorがないけどRuntimeでErrorになる可能性）→ あんまりよくないかも
-  addGuessTableRow(pokemon, doko);
-  // check if correct
+  // あっても、あわなくても、一旦Pokemonの情報を表に追加する。
+  // pokemon : どんなポケモンを
+  // inputPokemonInfoLocation : どこに表を追加するか
+  addGuessTableRow(pokemon, inputPokemonInfoLocation);
+
+  // Pokemonのidを見て、正しいかどうかを判断してAlertだけする。
   if (pokemon.id === correctPokemon) {
     alert(`Correct! The pokemon is ${pokemon.name}!`);
   } else {
     // add hint
-    // hint[上に追加されたrow数]
-    const hintIndex = doko.children.length
-      ? Math.min(doko.children.length - 1, hint.length - 1)
+    // hintは答えが間違ったとき、５回ヒントをする。
+    // 何番目の答えなのかは、inputPokemonInfoLocationの子要素の数で判断する。hint[上に追加されたrow数]
+    const hintIndex = inputPokemonInfoLocation.children.length
+      ? Math.min(inputPokemonInfoLocation.children.length - 1, hint.length - 1)
       : 0;
     alert(`Wrong! Here's a hint: ${hint[hintIndex]}`);
   }
 }
 
-function addGuessTableRow(pokemon: Pokemon, doko: HTMLElement) {
+function addGuessTableRow(
+  pokemon: Pokemon,
+  inputPokemonInfoLocation: HTMLElement,
+) {
   // <tr>
   //  <td>number</td>
   //   <td>name</td>
@@ -83,8 +94,11 @@ function addGuessTableRow(pokemon: Pokemon, doko: HTMLElement) {
   row.appendChild(numberData);
   row.appendChild(nameData);
   row.appendChild(typeData);
-  // dokoがあれば（null/undefinedじゃなければ）rowをdokoの子要素として追加する。
-  doko.appendChild(row);
+  // inputPokemonInfoLocationがあれば（null/undefinedじゃなければ）rowをinputPokemonInfoLocationの子要素として追加する。
+  inputPokemonInfoLocation.appendChild(row);
 }
 
 export { setupGuessFavorite };
+
+// Todo
+// 使用者が入力するときnumberだけ入力できるようにしたい
