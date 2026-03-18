15d66975babcc17c17e706c21079c0d9f690c1be
SonMyeongJin <smj2802@naver.com>
2026-03-18 18:10:47 +0900

    [Refactor] battle section部分 ：関数抽出 + コメント説明追加

--- src/widgets/battle/setupBattle.ts
+++ src/widgets/battle/setupBattle.ts
@@ -16,74 +16,82 @@ const skillPower: number[] = [1, 2, 3, 4, 1, 2, 3, 4];
 
 let blueTurn: boolean = true; // true: blue's turn, false: red's turn
 
-//querySelectorAll( CSS selctor ) : 해당하는 모든 요소를 NodeList 형태로 반환
-// 즉 id=zenigame 안에 있는 skill 클래스 안에 있는 button 요소들을 모두 가져와서 리스트로 blueBattleButtons에 저장
+// querySelectorAll( CSS selctor ) : 該当するすべての要素をNodeList形式で返す
+// つまり id=zenigame 内のスキルクラスにある button 要素をすべて取得し、リストとして blueBattleButtons に保存する
 const blueBattleButtons = document.querySelectorAll('#zenigame .skill button');
 const redBattleButtons = document.querySelectorAll('#hitokage .skill button');
 
 const redHpElement = document.getElementById('redHP'); // <span id="redHP">100</span>
 const hpListElement = redHpElement?.parentElement; // <li data-hp="100"><span id="redHP">100</span></li>
 
+// 一般化する必要がある。
 const zenigameSection = document.getElementById('zenigame');
 const hitokageSection = document.getElementById('hitokage');
 
-// id="blueHP" 인 요소를 가져와서
-const blueHpElement = document.getElementById('blueHP');
-// 그 요소의 부모를 찾아와서
-const blueHpListElement = blueHpElement?.parentElement;
+const blueHpElement = document.getElementById('blueHP'); // id="blueHP" の要素を取得して
+const blueHpListElement = blueHpElement?.parentElement; // その要素の親を取得して
 
 function setupBattle() {
   // [<button>たきのぼり</button>, <button>みずでっぽう</button>, <button>あわ</button>, <button>ハイドロポンプ</button>]
-  // forEach : 각 배열의 요소
+  // forEach : 各配列の要素
   blueBattleButtons.forEach((button, index) => {
-    button?.addEventListener('click', () => {
-      if (blueTurn) {
-        redHP -= HpCalculator(blueAttack, redDefense, skillPower[index]);
-        if (redHP < 0) {
-          redHP = 0;
-        } // HPがマイナスになるのを防ぐため
-
-        if (redHpElement?.innerText) {
-          // HTMLにHPを表示するために
-          redHpElement.innerText = redHP.toFixed(0).toString(); // toString() 無くでもいい。でもTextにNumberを入ればErrorになる。
-
-          // cssのために
-          if (hpListElement) {
-            //.dataset.hpは自動にdata-hp属性をつくてくれる。つまり、hpListElement 要素にdata-hp属性ができて、そこにredHPの値を入れる。
-            hpListElement.dataset.hp = redHP.toFixed(0).toString(); // tofixd => 小数点のために
-          }
-        }
-        battleEndCheck();
-        turnChange();
-      }
-    });
+    button?.addEventListener('click', () => onSkillClickHandler(index));
   });
-
   redBattleButtons.forEach((button, index) => {
-    button?.addEventListener('click', () => {
-      if (!blueTurn) {
-        blueHP -= HpCalculator(redAttack, blueDefense, skillPower[index]);
-        if (blueHP < 0) {
-          blueHP = 0;
-        } // HPがマイナスになるのを防ぐため
-
-        if (blueHpElement?.innerText) {
-          // HTMLにHPを表示するために
-          // <span id="blueHP"> ここが innerText </span>
-          blueHpElement.innerText = blueHP.toFixed(0).toString();
-
-          // cssのために
-          if (blueHpListElement) {
-            // dataset를 이용해 data-hp 속성에 접근해서 HP값을 넣고
-            // <li data-hp="100"><span id="blueHP">100</span></li>
-            blueHpListElement.dataset.hp = blueHP.toFixed(0).toString();
-          }
-        }
-        battleEndCheck();
-        turnChange();
-      }
-    });
+    button?.addEventListener('click', () => onSkillClickHandler(index));
   });
+}
+
+function onSkillClickHandler(index: number) {
+  // レッドターンならここに引っかかり
+  if (!blueTurn) {
+    // 該当スキルボタンのダメージを数値化してblueHPを削減する。
+    blueHP -= HpCalculator(redAttack, blueDefense, skillPower[index]);
+    // HPがマイナスになるのを防ぐため
+    if (blueHP < 0) {
+      blueHP = 0;
+    }
+
+    // HPをHTMLに表示するために
+    if (blueHpElement?.innerText) {
+      // HTMLにHPを表示するために
+      // <span id="blueHP"> ここが innerText </span>
+      blueHpElement.innerText = blueHP.toFixed(0).toString();
+
+      // cssのために
+      if (blueHpListElement) {
+        // datasetを利用してdata-hp属性にアクセスし、HP値を設定する
+        // <li data-hp="100"><span id="blueHP">100</span></li>
+        blueHpListElement.dataset.hp = blueHP.toFixed(0).toString();
+      }
+    }
+  }
+
+  // ブルートンならここに引っかかり
+  if (blueTurn) {
+    // 該当スキルボタンのダメージを数値化してredHPを削減する。
+    redHP -= HpCalculator(blueAttack, redDefense, skillPower[index]);
+    // HPがマイナスになるのを防ぐため
+    if (redHP < 0) {
+      redHP = 0;
+    }
+
+    // HPをHTMLに表示するために
+    if (redHpElement?.innerText) {
+      redHpElement.innerText = redHP.toFixed(0).toString(); // toString() 無くでもいい。でもTextにNumberを入ればErrorになる。
+
+      // cssのために
+      if (hpListElement) {
+        //.dataset.hpは自動にdata-hp属性をつくてくれる。つまり、hpListElement 要素にdata-hp属性ができて、そこにredHPの値を入れる。
+        hpListElement.dataset.hp = redHP.toFixed(0).toString(); // tofixd => 小数点のために
+      }
+    }
+  }
+
+  // 攻撃パートが終わったらバトル終了をチェックし、終わっていなければターンを渡す。
+  if (!battleEndCheck()) {
+    turnChange();
+  }
 }
 
 function HpCalculator(
@@ -98,15 +106,19 @@ function HpCalculator(
 function battleEndCheck() {
   if (blueHP <= 0) {
     alert('Red wins! The pokemon is ヒトカゲ !');
+    return true;
   }
   if (redHP <= 0) {
     alert('Blue wins! The pokemon is ゼニガメ !');
+    return true;
   }
+  return false;
 }
 
 function turnChange() {
   if (blueTurn) {
     blueTurn = false;
+    // ターンが変わるときに、攻撃できる方のセクションを有効にして、攻撃できない方のセクションを無効にする。
     zenigameSection?.classList.toggle('disabled');
     hitokageSection?.classList.toggle('disabled');
   } else {
