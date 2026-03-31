13794689db4a701ed108b620a24ee5ebabe54ca2
SonMyeongJin <smj2802@naver.com>
2026-03-31 15:46:23 +0900

    [Feat] renderReviewItem メーソド抽出

--- src/pages/components/renderReviewItem.ts
+++ src/pages/components/renderReviewItem.ts
@@ -1,3 +1,4 @@
+import type { ReviewItem } from 'src/domain/dto/getReviewResponse';
 import { getReviews } from 'src/features/getReviews';
 import { postReviewLike } from 'src/features/postReviewLike';
 
@@ -7,7 +8,6 @@ const getReviewRequest = {
   productId: '1234',
 };
 
-// biome-ignore lint/complexity/noExcessiveLinesPerFunction: test
 async function renderReviewItem() {
   const reviewSection = document.getElementById('review-list');
   if (!reviewSection) {
@@ -18,64 +18,146 @@ async function renderReviewItem() {
   const reviewUl = document.createElement('ul');
 
   data.reviews.forEach((item) => {
-    const reviewarticle = document.createElement('article');
-    const reviewerDiv = document.createElement('div');
-    reviewerDiv.classList.add('reviewer-info');
-    const reviewerImg = document.createElement('img');
-    reviewerImg.classList.add('reviewer-profile');
-    reviewerImg.src = item.reviewerImagePath;
-    reviewerImg.alt = `${item.reviewerName}'s profile picture`;
-    reviewerDiv.appendChild(reviewerImg);
-
-    const reviewerName = document.createElement('p');
-    reviewerName.textContent = item.reviewerName;
-    reviewerDiv.appendChild(reviewerName);
-    const reviewDiv = document.createElement('div');
-    const reviewRating = document.createElement('p');
-    reviewRating.textContent = '★'.repeat(item.rating);
-    reviewRating.classList.add('review-rating');
-    reviewDiv.appendChild(reviewRating);
-    const reviewContent = document.createElement('p');
-    reviewContent.textContent = item.content;
-    reviewContent.classList.add('review-content');
-    reviewDiv.appendChild(reviewContent);
-    reviewarticle.appendChild(reviewerDiv);
-    reviewarticle.appendChild(reviewDiv);
-    const hr = document.createElement('hr');
-    reviewarticle.appendChild(hr);
-    reviewUl.appendChild(reviewarticle);
-    const countLike = document.createElement('p');
-    countLike.classList.add('like-count-text');
-    countLike.textContent = 'いいね数:';
-    reviewDiv.appendChild(countLike);
-
-    const countLikeValue = document.createElement('span');
-    countLikeValue.classList.add('like-count');
-    const likeCount = item.likeCount;
-    countLikeValue.textContent = likeCount.toString();
-    countLike.appendChild(countLikeValue);
-
-    const likeButton = addLikeButton();
-    reviewDiv.appendChild(likeButton);
-    likeButton.onclick = async () => {
-      const successLike = await postReviewLike(item.reviewId, '2');
-      if (successLike) {
-        console.log(`Liked review ${item.reviewId}`);
-
-        likeButton.classList.add('btn', 'btn-warning', 'btn-sm', 'mt-2');
-
-        // 에니메이션 실행 (0.5초)
-        countLikeValue.classList.add('like-animation');
-        // 0.25 초 뒤에 좋아요수 증가
-        setTimeout(() => {
-          countLikeValue.textContent = (likeCount + 1).toString();
-        }, 250);
-        // 남은 0.25초 뒤에 에니메이션 제거
-      }
-    };
+    reviewUl.appendChild(createReviewArticle(item));
   });
   reviewSection.innerHTML = '';
   reviewSection.appendChild(reviewUl);
+}
+
+function createReviewArticle(item: ReviewItem) {
+  const reviewArticle = document.createElement('article');
+  const reviewerDiv = createReviewerDiv();
+  const reviewDiv = createReviewDiv();
+
+  reviewerDiv.appendChild(
+    createReviewerImg(item.reviewerImagePath, item.reviewerName),
+  );
+  reviewerDiv.appendChild(createReviewerName(item.reviewerName));
+
+  reviewDiv.appendChild(createReviewRating(item.rating));
+  reviewDiv.appendChild(createReviewContent(item.content));
+
+  const countLikeValue = createLikeCountValue(item.likeCount);
+  reviewDiv.appendChild(createLikeCount(countLikeValue));
+
+  const likeButton = addLikeButton();
+  bindLikeButton(likeButton, countLikeValue, item);
+  reviewDiv.appendChild(likeButton);
+
+  reviewArticle.appendChild(reviewerDiv);
+  reviewArticle.appendChild(reviewDiv);
+
+  const hr = document.createElement('hr');
+  reviewArticle.appendChild(hr);
+
+  return reviewArticle;
+}
+
+function createReviewerDiv() {
+  const reviewerDiv = document.createElement('div');
+  reviewerDiv.classList.add('reviewer-info');
+  return reviewerDiv;
+}
+
+function createReviewDiv() {
+  const reviewDiv = document.createElement('div');
+  return reviewDiv;
+}
+
+function createReviewerImg(imagePath: string, reviewerName: string) {
+  const reviewerImg = document.createElement('img');
+  reviewerImg.classList.add('reviewer-profile');
+  reviewerImg.src = imagePath;
+  reviewerImg.alt = `${reviewerName}'s profile picture`;
+  return reviewerImg;
+}
+
+function createReviewerName(reviewerName: string) {
+  const reviewerNameElement = document.createElement('p');
+  reviewerNameElement.textContent = reviewerName;
+  return reviewerNameElement;
+}
+
+function createReviewRating(rating: number) {
+  const reviewRating = document.createElement('p');
+  reviewRating.textContent = '★'.repeat(rating);
+  reviewRating.classList.add('review-rating');
+  return reviewRating;
+}
+
+function createReviewContent(content: string) {
+  const reviewContent = document.createElement('p');
+  reviewContent.textContent = content;
+  reviewContent.classList.add('review-content');
+  return reviewContent;
+}
+
+function createLikeCount(countLikeValue: HTMLSpanElement) {
+  const countLike = document.createElement('p');
+  countLike.classList.add('like-count-text');
+  countLike.textContent = 'いいね数:';
+
+  countLike.appendChild(countLikeValue);
+  return countLike;
+}
+
+function createLikeCountValue(likeCount: number) {
+  const countLikeValue = document.createElement('span');
+  countLikeValue.classList.add('like-count');
+  countLikeValue.textContent = likeCount.toString();
+  return countLikeValue;
+}
+function changeLiked(likeButton: HTMLButtonElement) {
+  likeButton.classList.add('liked');
+}
+function changeUnliked(likeButton: HTMLButtonElement) {
+  likeButton.classList.remove('liked');
+}
+function startLoading(likeButton: HTMLButtonElement) {
+  likeButton.classList.add('loading');
+}
+function completeLoading(likeButton: HTMLButtonElement) {
+  likeButton.classList.remove('loading');
+}
+
+function bindLikeButton(
+  likeButton: HTMLButtonElement,
+  countLikeValue: HTMLSpanElement,
+  item: ReviewItem,
+) {
+  let currentLikeCount = item.likeCount;
+
+  likeButton.onclick = async () => {
+    startLoading(likeButton);
+    const successLike = await postReviewLike(item.reviewId, '2').finally(() => {
+      completeLoading(likeButton);
+    });
+
+    if (successLike) {
+      changeLiked(likeButton);
+    } else {
+      changeUnliked(likeButton);
+    }
+
+    currentLikeCount += 1;
+
+    // boot strap
+    likeButton.classList.add('btn', 'btn-warning', 'btn-sm', 'mt-2');
+
+    countLikeValue.getBoundingClientRect();
+    countLikeValue.classList.add('like-animation');
+    setTimeout(() => {
+      countLikeValue.textContent = currentLikeCount.toString();
+    }, 250);
+    setTimeout(() => {
+      likeButton.classList.remove(
+        'like-animation,btn',
+        'btn-warning',
+        'btn-sm',
+        'mt-2',
+      );
+    }, 1000);
+  };
 }
 
 function addLikeButton() {
